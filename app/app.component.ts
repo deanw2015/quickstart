81ba9bb7ba249d56820aa48537e4afbd9b1e1df8
Dean Williams <dwilliams@elstatgroup.com>
2017-01-01 20:24:26 +0000

    Testing if commits are local

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,19 +5,39 @@ export class Hero {
 	name: string;
 }
 
+
+
+
 @Component({
-  selector: 'my-app',
+  selector: 'my-app', 
   template: `
 	  <h1>{{title}}</h1>
-	  <h2>{{hero.name}} details!</h2>
-	  <div><label>id: </label>{{hero.id}}</div>
-	  <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+	  <h2>My Heroes</h2>
+	  <ul>
+      <li *ngFor="let hero of heros">
+      <!-- each hero goes here -->
+      </li>
+    </ul>
 	  `,	 
 })
 export class AppComponent  { 
 	title = 'Tour of Heroes'; 
+  heros: HEROES;
 	hero: Hero = {
 		id: 1,
 		name: 'Windstorm'	
 	};
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
