809133241b96ae1061a8e51968fa8d6b65557fa1
deanw2015 <deanwilliams15@outlook.com>
2017-01-02 01:21:13 +0000

    Refactored AppComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+  providers:    [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
